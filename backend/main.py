fde76b4c94b645363ee4379843edd12da91b56f3
Jakka Prihatna <jprihatna@gmail.com>
2026-08-14 17:40:13 +0700

    feat(backend): add `/api/v1/trips` endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from services.trip_service import (
+    calculate_daily_budget,
+    get_trip_category
+)
+
+class TripRequest(BaseModel):
+    destination:    str
+    days:           int
+    budget:         float
 
 app = FastAPI()
 
@@ -16,3 +25,21 @@ def health():
     return {
         "status": "OK"
     }
+
+
+@app.post("/api/v1/trips")
+def create_trip(request: TripRequest):
+    daily_budget = calculate_daily_budget(
+        request.budget, request.days
+    )
+    category = get_trip_category(
+        request.budget
+    )
+    return {
+        "destination": request.destination,
+        "days": request.days,
+        "budget": request.budget,
+        "daily_budget": daily_budget,
+        "category": category
+    }
+
